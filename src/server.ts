c868fded8e0c7f2d21e3163dad9d3fcd8f6cc845
jonayeds <jonayeeds@gmail.com>
2025-06-26 16:51:23 +0600

    added unhandled and unCaught execption handler and closing down Project

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,33 @@ import config from "./config";
 
 const PORT = config.port || 8000;
 
+
+
 async function main() {
   const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  process.on("uncaughtException", (error) => {
+    if(server){
+      server.close(()=>{
+        console.info("Uncaught Exception: Shutting down server due to uncaught exception");
+      })
+    }
+    console.log("Uncaught Exception:", error);
+    process.exit(1);
+  })
+
+  process.on("unhandledRejection",(error)=>{
+    if(server){
+      server.close(()=>{
+        console.info("Unhandled Rejection: Shutting down server due to unhandled rejection");
+      })
+    }
+    console.error("Unhandled Rejection:", error);
+    process.exit(1);
+  })
+
 }
 
 main()
